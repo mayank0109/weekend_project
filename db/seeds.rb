5fdc833e75bd3e5a0f4b292a8d97204d8b1edf74
mayank0109 <mayankkhanna038@gmail.com>
2023-03-19 15:02:37 +0530

    add tests users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(first_name: "Jeff",
+            last_name: "Hardy",
+            email: "sample@google.com",
+            password: "welcome",
+            password_confirmation: "welcome")
+
+5.times do
+  User.create!(first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name,
+               email: Faker::Internet.email,
+               password: "welcome",
+               password_confirmation: "welcome")
+end
